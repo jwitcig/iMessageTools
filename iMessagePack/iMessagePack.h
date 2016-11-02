a6ef5bdacb625b6bfcf4976162fcd4d1bfd0c513
Jonah Witcig <jwitcig@gmail.com>
2016-11-01 23:46:18 -0500

    Initial Commit

---
+++ iMessagePack/iMessagePack.h
@@ -0,0 +1,19 @@
+//
+//  iMessagePack.h
+//  iMessagePack
+//
+//  Created by Jonah Witcig on 11/1/16.
+//  Copyright © 2016 Jonah Witcig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iMessagePack.
+FOUNDATION_EXPORT double iMessagePackVersionNumber;
+
+//! Project version string for iMessagePack.
+FOUNDATION_EXPORT const unsigned char iMessagePackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iMessagePack/PublicHeader.h>
+
+
